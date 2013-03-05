505f78f5165db99f9b8fd4e343a6393c03337a55
tlamer <tlamer@gmail.com>
2013-03-05 17:31:41 +0100

    init

---
+++ postgres_sqlalchemy/init.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS dim_customers;
+DROP TABLE IF EXISTS src_customers;
+DROP TABLE IF EXISTS tmp;
+
+CREATE TABLE src_customers (
+    id integer PRIMARY KEY,
+    name varchar(128),
+    address varchar(256)
+);
+
+CREATE TABLE dim_customers (
+    id integer,
+    name varchar(128),
+    address varchar(256),
+    surrogate SERIAL PRIMARY KEY,
+    valid_from timestamp,
+    valid_to timestamp
+);
+
+INSERT INTO src_customers (id, name, address) VALUES (1,'peter','kapicova');
+INSERT INTO src_customers (id, name, address) VALUES (2,'danko','romanova');
+INSERT INTO src_customers (id, name, address) VALUES (3,'stivi','foobar');
+
---
+++ postgres_sqlalchemy/scd.sql
@@ -0,0 +1,23 @@
+﻿-- new database entries
+INSERT INTO dim_customers
+    SELECT s.*,nextval('dim_customers_surrogate_seq'),CURRENT_TIMESTAMP
+    FROM (SELECT id FROM src_customers EXCEPT SELECT id FROM dim_customers) n
+    JOIN src_customers s ON n.id=s.id;
+
+-- temporary table with changes
+DROP TABLE IF EXISTS tmp;
+CREATE TABLE tmp AS
+    SELECT s.id, s.name, s.address, nextval('dim_customers_surrogate_seq'), CURRENT_TIMESTAMP
+    FROM src_customers s
+    JOIN dim_customers d ON s.id=d.id
+    WHERE d.valid_to IS NULL AND (d.name!=s.name OR d.address!=s.address);
+
+-- update timestamps for old entries
+UPDATE dim_customers d 
+    SET valid_to=CURRENT_TIMESTAMP 
+    FROM tmp t 
+    WHERE d.id=t.id And d.valid_to IS NULL;
+
+-- merge tables
+INSERT INTO dim_customers SELECT * FROM tmp;
+
